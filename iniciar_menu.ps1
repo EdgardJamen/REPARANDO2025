eddb12c479b6f34ba04156f493b07729bd1b100d
EdgardJamen <gabrielopenc@gmail.com>
2025-04-19 16:38:33 -0300

    Update iniciar_menu.ps1

--- iniciar_menu.ps1
+++ iniciar_menu.ps1
@@ -1,16 +1,8 @@
-Write-Host "Iniciando programa..." -ForegroundColor Yellow
-
-# Ruta del menú en Google Drive
-$menuUrl = "https://drive.google.com/file/d/1A7eZ_l0stvm6VkGUwfE8mZxILUK2qBz1/view?usp=sharing"
-
-# Guardar el archivo en la carpeta temporal del sistema
+$menuUrl = "https://raw.githubusercontent.com/EdgardJamen/REPARANDO2025/main/menu.ps1"
 $menuPath = "$env:TEMP\menu.ps1"
+
+# Descargar el script desde GitHub
 Invoke-WebRequest -Uri $menuUrl -OutFile $menuPath
 
-# Verificar si la descarga fue exitosa
-if (Test-Path $menuPath) {
-    Write-Host "Ejecución de menu.ps1..." -ForegroundColor Green
-    Start-Process -FilePath "powershell.exe" -ArgumentList "-ExecutionPolicy Bypass -File $menuPath" -Wait
-} else {
-    Write-Host "Error: No se pudo descargar menu.ps1." -ForegroundColor Red
-}
+# Ejecutar el script descargado
+Start-Process -FilePath "powershell.exe" -ArgumentList "-ExecutionPolicy Bypass -File $menuPath" -Wait
