1b2efda7af92fd778ee75d2439a112681ac6877c
EdgardJamen <gabrielopenc@gmail.com>
2025-04-20 16:36:43 -0300

    Update CrearPuntoRestauracion.ps1

--- CrearPuntoRestauracion.ps1
+++ CrearPuntoRestauracion.ps1
@@ -1,13 +1,13 @@
 [Console]::OutputEncoding = [System.Text.Encoding]::UTF8
 
-# Función para comprobar si se ejecuta como administrador
+# Funcion para comprobar si se ejecuta como administrador
 function Test-Admin {
     $currentIdentity = [Security.Principal.WindowsIdentity]::GetCurrent()
     $principal = New-Object Security.Principal.WindowsPrincipal($currentIdentity)
     return $principal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
 }
 
-# Si NO se está ejecutando como administrador, reinicia el script con elevación.
+# Si NO se esta ejecutando como administrador, reinicia el script con elevacion.
 if (-not (Test-Admin)) {
     Write-Host "Este script requiere privilegios de administrador. Reiniciando con elevacion..." -ForegroundColor Yellow
     Start-Process powershell.exe -ArgumentList "-ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
@@ -15,8 +15,12 @@ if (-not (Test-Admin)) {
 }
 
 try {
+    # Activar el servicio de restauracion del sistema antes de ejecutar
+    Write-Host "Activando el servicio de restauracion del sistema..." -ForegroundColor Yellow
+    Start-Service -Name "VSS" -ErrorAction SilentlyContinue
+
+    # Crear el punto de restauracion
     Write-Host "Creando un punto de restauracion. Por favor espere..." -ForegroundColor Cyan
-    # Se crea el punto de restauracion con una descripción personalizada.
     Checkpoint-Computer -Description "Backup Pre-Optimizacion" -RestorePointType MODIFY_SETTINGS -ErrorAction Stop
     Write-Host "Punto de restauracion creado exitosamente." -ForegroundColor Green
 }
@@ -26,3 +30,4 @@ catch {
 
 Write-Host "Presiona Enter para continuar..."
 Read-Host
+
